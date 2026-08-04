0907773a235d99aefaa862a7065f9212def66e84
hsnandurkar <134283310+hsnandurkar@users.noreply.github.com>
2026-08-04 09:44:54 +0530

    Comment out Terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "statefilelockbucket"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "statefilelockbucket"
+#     key     = "terraform.tfstate"
+#     region  = "us-east-1"
+#     encrypt = true
+#   }
+# }
